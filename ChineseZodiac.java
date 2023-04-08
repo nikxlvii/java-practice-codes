8c69c472a8b832bd55b260b1cf7bdf0daad8286a
Nik Joshi <71061671+nikxlvii@users.noreply.github.com>
2023-04-08 19:20:14 +0530

    Add files via upload

---
+++ ChineseZodiac.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ChineseZodiac {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter a year: ");
+        int year = input.nextInt();
+
+        switch(year % 12){
+            case 0: System.out.print("Monkey"); break;
+            case 1: System.out.print("Rooster"); break;
+            case 2: System.out.print("Dog"); break;
+            case 3: System.out.print("Pig"); break;
+            case 4: System.out.print("Rat"); break;
+            case 5: System.out.print("Ox"); break;
+            case 6: System.out.print("Tiger"); break;
+            case 7: System.out.print("Rabbit"); break;
+            case 8: System.out.print("Dragon"); break;
+            case 9: System.out.print("Snake"); break;
+            case 10: System.out.print("Horse"); break;
+            case 11: System.out.print("Sheep"); break;
+        }
+        input.close();
+    }
+}
